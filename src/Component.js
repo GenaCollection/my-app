e533c2f91f00524a0efd7a5f1624a2cf581e75b2
GenaCollection <gena.emiryan91@gmail.com>
2022-10-25 20:18:52 +0400

    123

---
+++ src/Component.js
@@ -0,0 +1,9 @@
+import  avatar from './Avatar'
+import  header from './Header'
+
+function CreateHeader (){
+    return (
+        <div>{header}{avatar}</div>
+     );
+}
+export default CreateHeader
